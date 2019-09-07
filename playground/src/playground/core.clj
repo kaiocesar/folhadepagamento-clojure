289f55882c63e79eb03e27211120da35469be43e
Kaio Cesar <tecnico.kaio@gmail.com>
2019-09-06 21:49:42 -0300

    clausula if

--- playground/src/playground/core.clj
+++ playground/src/playground/core.clj
@@ -1,17 +1,21 @@
 (ns playground.core)
 
-(defn valor-descontado [valor-bruto]
+(defn valor-descontado
   "Valor com 10% de desconto"
-  (let [desconto (/ 10 100)]
-    (* valor-bruto (- 1 0.10))))
-
+  [valor-bruto]
+  (if (> valor-bruto 5000)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto         (* valor-bruto taxa-de-desconto)]
+      (desconto))
+    (- valor-bruto desconto)))
 
 (defn -main
   "Estoque de produtos"
   []
-  (def estoque ["Notebook", "Teclado", "Mouse", "Monitor"])
+  (def estoque ["Notebook", "Teclado", "Mouse", "Monitor", "Impressora"])
   (def valor-bruto-estoque 5300)
   (def estoque (conj estoque "Mousepad"))
   (println "Total de produtos:" (count estoque))
   (println "Estoque:" estoque)
-  (println "Valor descontado" (valor-descontado valor-bruto-estoque)))
+  (println "Valor final:" (valor-descontado valor-bruto-estoque)))
+
