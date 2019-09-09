9de22aecbab5ff44bc76a29f4606808bde63af19
Kaio Cesar <tecnico.kaio@gmail.com>
2019-09-08 21:13:36 -0300

    fix do let

--- playground/src/playground/core.clj
+++ playground/src/playground/core.clj
@@ -1,21 +1,23 @@
 (ns playground.core)
 
-(defn valor-descontado
-  "Valor com 10% de desconto"
-  [valor-bruto]
-  (if (> valor-bruto 5000)
-    (let [taxa-de-desconto (/ 10 100)
-          desconto         (* valor-bruto taxa-de-desconto)]
-      (desconto))
-    (- valor-bruto desconto)))
 
 (defn -main
   "Estoque de produtos"
   []
+  (defn valor-descontado
+    "Valor com 10% de desconto"
+    [valor-bruto]    
+      (if (< valor-bruto 500)
+        (let [taxa-de-desconto (/ 10 100)]
+          (- valor-bruto (* valor-bruto taxa-de-desconto)))
+        valor-bruto)
+  )
+
   (def estoque ["Notebook", "Teclado", "Mouse", "Monitor", "Impressora"])
-  (def valor-bruto-estoque 5300)
+  (def valor-bruto-estoque 53)
   (def estoque (conj estoque "Mousepad"))
   (println "Total de produtos:" (count estoque))
   (println "Estoque:" estoque)
-  (println "Valor final:" (valor-descontado valor-bruto-estoque)))
+  (println "Valor final:" (valor-descontado valor-bruto-estoque))  
+  )
 
