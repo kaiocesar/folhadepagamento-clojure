cf986be20484745ed8c0ff65c69526962ec8ef46
Kaio Cesar <tecnico.kaio@gmail.com>
2019-09-10 16:58:09 -0300

    calculo do INSS

--- playground/src/playground/core.clj
+++ playground/src/playground/core.clj
@@ -1,11 +1,26 @@
 (ns playground.core)
 
+
+(defn aplicar-desconto [valor-desconto-porc valor]
+  "aplicar o desconto proporcinal"
+  (* valor (/ valor-desconto-porc 100)))
+
+
+(defn calcular-inss [salario_base]
+   "calculo inss"
+    (cond 
+      (< salario_base 1751.81) (aplicar-desconto 8 salario_base)
+      (and (>= salario_base 1751.82) (<= salario_base 2919.72)) (aplicar-desconto 9 salario_base)
+      (and (>= salario_base 2919.73) (<= salario_base 5839.45)) (aplicar-desconto 11 salario_base)
+      :else 642.34))
+
+
 (defn -main
   "Estoque de produtos"
   ; calculo baseando em https://www.jornalcontabil.com.br/folha-de-pagamento-entenda-como-fazer-o-calculo/
   []
   (def funcionarios {:1 {
-    :salario_bruto 3000 
+    :salario_base 3000 
     :base_horas 220
     :jornada {:inicio 8 :refeicao 60 :termino 17 }
     :insalubridade "média"
@@ -25,6 +40,8 @@
     ; calcular o vale alimentação
     ; adicional noturno
     ; adicional de insalubridade
-  (-> funcionarios :1 :salario_bruto println)
+  
+    (println (calcular-inss (-> funcionarios :1 :salario_base)))
+  
   (println "Folha de pagamento"))
 
--- playground/test/playground/core_test.clj
+++ playground/test/playground/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [playground.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest test-numbers
+  (is (= 1 1)))
